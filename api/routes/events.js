9997f8e8cf3191e622bfb907996a29cf30008ecb
malekannabi <malekannabi5@gmail.com>
2021-06-30 14:02:32 +0200

    fixed events

--- api/routes/events.js
+++ api/routes/events.js
@@ -2,7 +2,7 @@ const eventController=require('../controllers/eventController');
 
 // routes extraction + function association for every route
 module.exports=(app)=>{
-    app.get('/event',eventController.findAllEvents)
+    app.get('/event',eventController.events_get_all)
     app.get('/event/:id',eventController.getEvent)
     app.post('/event',eventController.createEvent);
     app.patch('/event/:id',eventController.updateEvent)
